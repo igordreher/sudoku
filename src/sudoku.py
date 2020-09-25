53a60e62d87a9d5c30d698b30815480b8f11a910
igordreher <igord.sparrenberger@gmail.com>
2020-09-25 19:15:18 -0300

    generate puzzle method

--- src/sudoku.py
+++ src/sudoku.py
@@ -11,27 +11,59 @@ class Sudoku:
         return str(np.array(self.grid))
 
     @staticmethod
-    def mk_grid():
-        grid = []
-        for _ in range(9):
-            grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-        return grid
+    def mk_grid(grid=None):
+        new_grid = []
+        if not grid:
+            for _ in range(9):
+                new_grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+        else:
+            for row in grid:
+                new_grid.append(row[:])
+        return new_grid
 
-    def fill_grid(self, random=False):
+    def fill_grid(self, random=True, solve=True):
         lst = list(range(1, 10))
-        for y in range(9):
-            for x in range(9):
-                if self.grid[y][x] == 0:
-                    if random:
-                        rd.shuffle(lst)
-                    for n in lst:
-                        if self.possible(y, x, n):
-                            self.grid[y][x] = n
-                            if self.fill_grid():
-                                return True
-                            self.grid[y][x] = 0
-                    return False
-        return True
+        if solve:
+            grid = self.grid
+        else:
+            grid = self.mk_grid(self.grid)
+
+        def loop(random, solve):
+            for y in range(9):
+                for x in range(9):
+                    if grid[y][x] == 0:
+                        if random:
+                            rd.shuffle(lst)
+                        for n in lst:
+                            if self.possible(y, x, n):
+                                grid[y][x] = n
+
+                                if loop(random, solve):
+                                    return True
+
+                                grid[y][x] = 0
+                        return False
+            return True
+
+        return loop(random, solve)
+
+    def gen_puzzle(self, clears=5):
+
+        def loop():
+            nonlocal clears
+            while clears > 0:
+                y = rd.randint(0, 8)
+                x = rd.randint(0, 8)
+                if self.grid[y][x] != 0:
+                    n = self.grid[y][x]
+                    self.grid[y][x] = 0
+                    clears -= 1
+                    if self.fill_grid(False, False):
+                        loop()
+                    else:
+                        self.grid[y][x] = n
+                        clears += 1
+        loop()
 
     def possible(self, y, x, n):
         for i in range(9):
