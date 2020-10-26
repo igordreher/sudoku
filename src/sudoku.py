f3df8ceaf8da2275b796e12cce8835984fe5c1d7
igordreher <igord.sparrenberger@gmail.com>
2020-10-26 18:16:51 -0300

    Update: cleaned sudoku class

--- src/sudoku.py
+++ src/sudoku.py
@@ -4,83 +4,74 @@ import random as rd
 
 class Sudoku:
 
+    EMPTY_CELL = 0
+
     def __init__(self):
-        self.grid = self.mk_grid()
-        self.gen_puzzle()
+        self._empty_grid()
 
     def __repr__(self):
         return str(np.array(self.grid))
 
+    def create_puzzle(self, cells_to_clear=20):
+        self._empty_grid()
+        self.fill_grid()
+        self._clear_cells(cells_to_clear)
+
+    def _empty_grid(self):
+        self.grid = []
+        for _ in range(9):
+            self.grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+
     @staticmethod
-    def mk_grid(grid=None):
-        new_grid = []
-        if not grid:
-            for _ in range(9):
-                new_grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-        else:
-            for row in grid:
-                new_grid.append(row[:])
-        return new_grid
+    def _copy_grid(grid):
+        grid_copy = []
+        for row in grid:
+            grid_copy.append(row[:])
 
-    def fill_grid(self, random=True, solve=True):
-        lst = list(range(1, 10))
-        if solve:
-            grid = self.grid
-        else:
-            grid = self.mk_grid(self.grid)
+        return grid_copy
 
-        def loop(random, solve):
+    def fill_grid(self):
+        possible_values = list(range(1, 10))
+
+        def loop():
             for y in range(9):
                 for x in range(9):
-                    if grid[y][x] == 0:
-                        if random:
-                            rd.shuffle(lst)
-                        for n in lst:
-                            if self.possible(y, x, n):
-                                grid[y][x] = n
+                    if self.grid[y][x] == self.EMPTY_CELL:
+                        rd.shuffle(possible_values)
+                        for value in possible_values:
+                            if self.is_value_possible(y, x, value):
+                                self.grid[y][x] = value
 
-                                if loop(random, solve):
+                                if loop():
                                     return True
 
-                                grid[y][x] = 0
+                                self.grid[y][x] = self.EMPTY_CELL
                         return False
             return True
 
-        return loop(random, solve)
-
-    def gen_puzzle(self, clears=20):
-        self.grid = self.mk_grid()
-        self.fill_grid()
-
-        def loop():
-            nonlocal clears
-            while clears > 0:
-                y = rd.randint(0, 8)
-                x = rd.randint(0, 8)
-                if self.grid[y][x] != 0:
-                    n = self.grid[y][x]
-                    self.grid[y][x] = 0
-                    clears -= 1
-                    if self.fill_grid(False, False):
-                        loop()
-                    else:
-                        self.grid[y][x] = n
-                        clears += 1
         loop()
 
-    def possible(self, y, x, n):
-        for i in range(9):
-            if self.grid[y][i] == n:
+    def _clear_cells(self, cells_to_clear):
+        while cells_to_clear > 0:
+            y = rd.randint(0, 8)
+            x = rd.randint(0, 8)
+            if self.grid[y][x] != self.EMPTY_CELL:
+                self.grid[y][x] = self.EMPTY_CELL
+                cells_to_clear -= 1
+
+    def is_value_possible(self, cell_y, cell_x, cell_value):
+        for x in range(9):
+            if self.grid[cell_y][x] == cell_value:
                 return False
-        for i in range(9):
-            if self.grid[i][x] == n:
+        for y in range(9):
+            if self.grid[y][cell_x] == cell_value:
                 return False
 
-        x_sqr = (x//3)*3
-        y_sqr = (y//3)*3
+        cell_x_sqr = (cell_x//3)*3
+        cell_y_sqr = (cell_y//3)*3
 
-        for i in range(0, 3):
-            for j in range(0, 3):
-                if self.grid[y_sqr+i][x_sqr+j] == n:
+        for y in range(0, 3):
+            for x in range(0, 3):
+                if self.grid[cell_y_sqr+y][cell_x_sqr+x] == cell_value:
                     return False
         return True
