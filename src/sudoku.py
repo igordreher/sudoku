d566187b4eac089f8bf0017454f694a00bf67e3a
igordreher <igord.sparrenberger@gmail.com>
2020-11-09 11:04:13 -0300

    Update sudoku class

--- src/sudoku.py
+++ src/sudoku.py
@@ -5,7 +5,6 @@ import random
 class Sudoku:
 
     EMPTY_CELL = 0
-    EMPTY_ROW = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     def __init__(self):
         self._clear_grid()
@@ -13,24 +12,15 @@ class Sudoku:
     def __repr__(self):
         return str(numpy.array(self.grid))
 
-    def generate_puzzle(self, n_of_blank_cells=20):
+    def generate_new_puzzle(self, n_of_blank_cells=20):
         self._clear_grid()
         self.fill_grid()
         self._clear_random_cells(n_of_blank_cells)
-        return self.grid
 
     def _clear_grid(self):
         self.grid = []
         for _ in range(9):
-            self.grid.append(self.EMPTY_ROW)
-        return self.grid
-
-    def _copy_grid(self):
-        grid_copy = []
-        for row in self.grid:
-            grid_copy.append(row[:])
-
-        return grid_copy
+            self.grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
 
     def fill_grid(self):
         possible_values = list(range(1, 10))
@@ -42,8 +32,8 @@ class Sudoku:
                         random.shuffle(possible_values)
                         cell_position = (x, y)
                         for value in possible_values:
-                            
-                            if self.is_value_possible(cell_position, value):
+
+                            if self.is_value_possible(value, cell_position):
                                 self.grid[y][x] = value
 
                                 if loop():
@@ -55,37 +45,39 @@ class Sudoku:
 
         loop()
 
-    def is_value_possible(self, cell_position, value):
+    def is_value_possible(self, value, cell_position):
         cell_x, cell_y = cell_position
 
-        if self._is_value_in_row(cell_y, value):
+        if self._is_value_in_row(value, cell_y):
             return False
 
-        if self._is_value_in_column(cell_x, value):
+        if self._is_value_in_column(value, cell_x):
             return False
 
-        if self._is_value_in_cell_box(cell_position, value):
+        if self._is_value_in_cell_box(value, cell_position):
             return False
 
-    def _is_value_in_row(self, row, value):
+        return True
+
+    def _is_value_in_row(self, value, row):
         if value in self.grid[row]:
             return True
         return False
 
-    def _is_value_in_column(self, column, value):
+    def _is_value_in_column(self, value, column):
         rows = range(9)
         for row in rows:
             if self.grid[row][column] == value:
                 return True
         return False
 
-    def _is_value_in_cell_box(self, cell_position, value):
+    def _is_value_in_cell_box(self, value, cell_position):
         cell_x, cell_y = cell_position
         box_x = (cell_x//3)*3
         box_y = (cell_y//3)*3
 
-        for y in range(0, 3):
-            for x in range(0, 3):
+        for y in range(3):
+            for x in range(3):
                 if self.grid[box_y+y][box_x+x] == value:
                     return True
         return False
